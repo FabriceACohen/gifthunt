004d643db8413c78bddaaa2daab7bfc894396991
FabriceACohen <fabrice.cohen@lesanico.fr>
2025-07-29 16:55:34 +0200

    feat: Step 5 - Firebase configured and initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gifthunt/src/presentation/routing/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:gifthunt/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
